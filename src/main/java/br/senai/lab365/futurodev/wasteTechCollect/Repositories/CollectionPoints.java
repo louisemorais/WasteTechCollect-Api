ac578547e5cbfe25121b61f1dd3f3794c6184685
Louise Morais <louisemorais3@gmail.com>
2025-04-22 16:59:42 -0300

    feat: criação do repository de CollectionPoints

---
+++ src/main/java/br/senai/lab365/futurodev/wasteTechCollect/Repositories/CollectionPoints.java
@@ -0,0 +1,6 @@
+package br.senai.lab365.futurodev.wasteTechCollect.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollectionPoints extends JpaRepository<CollectionPoints, Long> {
+}
