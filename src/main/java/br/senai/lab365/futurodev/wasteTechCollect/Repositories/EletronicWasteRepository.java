195b96eaaa322da7f988508e5d98a723af06ccbc
Louise Morais <louisemorais3@gmail.com>
2025-04-22 16:53:13 -0300

    feat: comitando mudanças

--- src/main/java/br/senai/lab365/futurodev/wasteTechCollect/Repositories/EletronicWasteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.senai.lab365.futurodev.wasteTechCollect.Repositories;
-
-import br.senai.lab365.futurodev.wasteTechCollect.models.EletronicWaste;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EletronicWasteRepository extends JpaRepository<EletronicWaste, Long> {
-}
